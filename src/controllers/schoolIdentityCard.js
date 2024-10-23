194d3eca1b5f5288e4f0ba96c5f53f20bde3f814
Juan Daniel <juandaniel312000@gmail.com>
2024-10-22 18:50:09 -0600

    Se actualizo la api para consultar info de credenciales

--- src/controllers/schoolIdentityCard.js
+++ src/controllers/schoolIdentityCard.js
@@ -3,8 +3,13 @@ const {getCurrentDate, normalizeDate } = require('../utils/getDate');
 
 const infoIdentityCard = (req, res) => {
     const idPerson = req.params.id;
-    SchoolIdentityCard.getIdInfoById(idPerson, (err, data) => {
+    SchoolIdentityCard.getIdInfoById(idPerson, (err, info) => {
         if(err) return res.status(500).json({success:false, message:'Error al consultar la información de la credencial', error:err});
+        let data = {
+            ...info[0],
+            isActive:info.length ? true : false
+        }
+        if(!data.isActive) data.idPerson = parseInt(idPerson);
         res.status(200).json({
             success:true,
             message:'Información escolar',
